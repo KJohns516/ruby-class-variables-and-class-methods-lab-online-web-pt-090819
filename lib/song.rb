d9217e24dc0b1b66844d846ca8202481f49ca9a9
Kristan Johns <kristandjohns@gmail.com>
2019-10-04 20:48:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+
+attr_accessor :lucifer
+
+@@count = 0
+@@song = 0
+@@genre = 0
+
+end
+
+def count 
+end 
+
+def artists
+end 
+
+def genres 
+end 
+
+def genre_count
+end 
+
+def artist_count
+end 
+
+# Song.new("Lucifer", "Jay-Z", "rap")
+Song.new("Lucifer")
+
+# song.count
+# song.artists 
+# song.genres 
+# song.genre_count
+# song.artist_count
